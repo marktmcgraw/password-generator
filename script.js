70c94c8939566f9c0ef59bff635134b6c816575e
Cotton Top <mtmcgr@gmail.com>
2020-01-14 09:15:12 -0600

    writing js file

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const numericEl= document.getElementById('numeric');
 const specialEl= document.getElementById('special'); 
 const createEl= document.getElementById('create');
 const resultEl= document.getElementById('result');
+
+
+
+createEl.addEventListener('click', () => {
+
+    const length = +lengthEl.value;
+	const hasUpper = uppercaseEl.checked;
+	const hasLower = lowercaseEl.checked;
+	const hasNumeric = numericEl.checked;
+	const hasSpecial = specialEl.checked;
+
+
+
+resultEl.innerHTML = createPassword(
+    hasUpper, hasLower, hasNumeric, hasSpecial,length);
+});
+
+
+
